b6449ad525a8b506c39ee9869eda183258fbe995
soni12-wp <kmsoni.db12@gmail.com>
2025-07-19 23:46:52 +0530

    new update5

--- Aws-project/main.tf
+++ Aws-project/main.tf
@@ -27,7 +27,7 @@ id = "sg-07d541efebb4806fb"
 }
 resource "aws_subnet" "public_subnet" {
   vpc_id     = data.aws_vpc.existing_vpc.id
-  cidr_block = "172.31.28.0/20"
+  cidr_block = "172.31.48.0/20"
   availability_zone = "eu-north-1c"
   map_public_ip_on_launch = true
 
