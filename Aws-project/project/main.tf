02c6e8821c156704778ef69cfbd2c2cb1a3da810
soni12-wp <kmsoni.db12@gmail.com>
2025-07-20 19:28:23 +0530

    new commit

--- Aws-project/project/main.tf
+++ Aws-project/project/main.tf
@@ -7,14 +7,14 @@
     region = "eu-north-1"
   }
   }
-
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.94.1"
     }
   }
-
+}
 provider "aws" {
   region = "eu-north-1"
 
